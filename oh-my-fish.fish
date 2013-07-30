38cc5f7cfc31d0aca4655ca81dd5b08a426ef022
Zitao <caoer115@gmail.com>
2013-07-29 20:24:56 -0400

    custom with git

--- oh-my-fish.fish
+++ oh-my-fish.fish
@@ -75,3 +75,56 @@ end
 
 # Re-adding user's functions so they have the highest priority
 set fish_function_path $user_function_path $fish_function_path
+
+
+# git.plugin.zsh
+# Aliases
+alias g='git'
+alias gst='git status'
+alias gl='git pull'
+alias gup='git pull --rebase'
+alias gp='git push'
+alias gd='git diff'
+alias gc='git commit -v'
+alias gca='git commit -v -a'
+alias gco='git checkout'
+alias gcm='git checkout master'
+alias gr='git remote'
+alias grv='git remote -v'
+alias grmv='git remote rename'
+alias grrm='git remote remove'
+alias grset='git remote set-url'
+alias grup='git remote update'
+alias gb='git branch'
+alias gba='git branch -a'
+alias gcount='git shortlog -sn'
+alias gcl='git config --list'
+alias gcp='git cherry-pick'
+alias glg='git log --stat --max-count=5'
+alias glgg='git log --graph --max-count=5'
+alias glgga='git log --graph --decorate --all'
+alias gss='git status -s'
+alias ga='git add'
+alias gm='git merge'
+alias grh='git reset HEAD'
+alias grhh='git reset HEAD --hard'
+alias gwc='git whatchanged -p --abbrev-commit --pretty=medium'
+alias gf='git ls-files | grep'
+alias gpoat='git push origin --all; and git push origin --tags'
+
+# Will cd into the top of the current repository
+# or submodule.
+alias grt='cd (git rev-parse --show-toplevel; or echo ".")'
+
+# Git and svn mix
+alias git-svn-dcommit-push='git svn dcommit; and git push github master:svntrunk'
+
+alias gsr='git svn rebase'
+alias gsd='git svn dcommit'
+
+# these aliases take advantage of the previous function
+alias ggpull='git pull origin (current_branch)'
+alias ggpush='git push origin (current_branch)'
+alias ggpnp='git pull origin (current_branch); and git push origin (current_branch)'
+
+# vi: ft=conf
